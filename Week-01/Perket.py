528ce4274b6c60181505e48b8f82ccfa730d7d1e
Nikolaj <nals@itu.dk>
2026-08-27 18:25:12 +0200

    Update Perket.py

--- Week-01/Perket.py
+++ Week-01/Perket.py
@@ -1,15 +1,21 @@
 # Thanks to RALC for pointing me to itertools and giving an example
     
 import itertools
+import sys
 
 
+bestAbsolute = sys.maxsize
 N = input()
 combinations = []
 ingredients = []
 
+# Get all ingredients into collection of tuples
 for x in range(n):
     ingredients[x] = tuple(input.split(" "))
 
+# Get all combinations
 for r in range(1, len(ingredients) +1):
     combinations.extend(itertools.combinations(ingredients, r))
-print (combinations)
+
+# Evaluate collection of combinations and find best absolute difference
+

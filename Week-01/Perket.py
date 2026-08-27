6217b99c58a57aa2005ade8012873c2b0ca99bcd
Nikolaj <nals@itu.dk>
2026-08-27 18:30:05 +0200

    Update Perket.py
    Outline for solution but my python syntax knowledge is bad

--- Week-01/Perket.py
+++ Week-01/Perket.py
@@ -8,9 +8,11 @@ bestAbsolute = sys.maxsize
 N = input()
 combinations = []
 ingredients = []
+prodSour = 0
+sumBitter = 0
 
 # Get all ingredients into collection of tuples
-for x in range(n):
+for x in range(N):
     ingredients[x] = tuple(input.split(" "))
 
 # Get all combinations
@@ -18,4 +20,8 @@ for r in range(1, len(ingredients) +1):
     combinations.extend(itertools.combinations(ingredients, r))
 
 # Evaluate collection of combinations and find best absolute difference
-
+for combination in combinations:
+    prodSour *= combination[1]
+    sumBitter += combination[1]
+    difference = prodSour - sumBitter
+    if difference < bestAbsolute: bestAbsolute = difference

179f4d598537f3232ef9656d0fa9bcd3d6e7cce6
Nikolaj <nals@itu.dk>
2026-08-27 15:12:14 +0200

    Fix BPTwo parsing and case behaviors
    Reworked to correctly read input and execute all five `t` cases as intended

--- Prerequisites/BPTwo.py
+++ Prerequisites/BPTwo.py
@@ -1,41 +1,45 @@
-N = input
-t = input
-numList = list
-
-for x in range(N):
-    numList.append(x)
+N, t = map(int, input().split())
+numList = list(map(int, input().split()))
 
 match t:
     case 1:
         lucky = False
-        numSet = numList
+        numSet = set(numList)
+
         for num in numSet:
             complement = 7777-num
-            if complement in numSet: lucky = True
+            if complement in numSet: 
+                lucky = True
+                break
         if lucky: print("Yes") 
         else: print("No")
+
     case 2:
-        numSet = numList
+        numSet = set(numList)
+
         if len(numSet) != len(numList):
-            print("Contains duplicates")
+            print("Contains duplicate")
         else:
             print("Unique")
+
     case 3:
-        numList.sort
+        numList.sort()
         size = len(numList)
-        if numList[size/2] == numList[0]: print(numList[0])
+        midVal = numList[size // 2]
+
+        if numList.count(midVal) > size // 2: print(midVal)
         else: print(-1)
+
     case 4:
-        numList.sort
+        numList.sort()
         size = len(numList)
         if size % 2 == 0:
-            print(numList[size] + " ")
-            print(numList[size-1])
+            print(numList[size // 2], numList[size // 2 - 1])
         else:
-            print(numList[size-1])
+            print(numList[size // 2 - 1])
+
     case 5:
-        filteredList = list(filter(lambda x: x < 100 or x > 999, numList))
-        filteredList.sort
-        for num in filteredList:
-            print(num + " ")
+        filteredList = list(filter(lambda x: 100 <= x <= 999, numList))
+        filteredList.sort()
+        print(*filteredList)
 

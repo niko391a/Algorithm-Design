42b8e8aa6998158b694e4f30cbc95f73f94d7dbe
Nikolaj <nals@itu.dk>
2026-09-02 19:02:15 +0200

    Update Backspace.py

--- Prerequisites/Hello_Kattis/Backspace.py
+++ Prerequisites/Hello_Kattis/Backspace.py
@@ -4,9 +4,7 @@ q = []
 for i in range(len(x)):
     if x[i] == '<':
         y = q.pop()
-        print("Popped: " + str(y))
     else: 
         q.append(x[i])
-        print("Pushed: " + x[i])
 
 print("".join(q))

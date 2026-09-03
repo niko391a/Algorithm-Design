51660a040a1925a6a96d448930de1e6370cee4c6
Nikolaj Alsøe <nals@itu.dk>
2026-09-03 11:38:38 +0200

    Create match.py

---
+++ Week-02/match.py
@@ -0,0 +1,31 @@
+# Thank you to RALC for suggesting a set of remaining suitors(proposers)
+
+N, m = map(int, input().split())
+
+proposers = {} 
+rejectors = {}
+matches = {}
+
+# Proposors
+for _ in range(N // 2):
+    parts = input().split()
+    name = parts[0]
+    proposers[name] = parts[1:]
+
+# Rejectors
+for _ in range(N // 2):
+    parts = input().split()
+    name = parts[0]
+    rejectors[name] = parts[1:]
+
+unmatched = set(proposers)
+
+# Pair sim
+while len(unmatched) != 0:
+    currentProposer = unmatched.pop()
+    currentPref = proposers[currentProposer].pop(0)
+
+    
+
+
+

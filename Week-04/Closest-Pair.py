0d83d0ee2a7ae50c8972beaae38a0e60f8bb474a
Nikolaj <nals@itu.dk>
2026-09-18 14:46:05 +0200

    Update Closest-Pair.py

--- Week-04/Closest-Pair.py
+++ Week-04/Closest-Pair.py
@@ -1,15 +1,19 @@
 n = int(input())
 
 points = []
-closestPair = []
 
 for _ in range(n):
     point = tuple(map(float, input().split()))
     points.append(point)
 
 # Sort by x
-points.sort(key=lambda x: x[1])
+points.sort()
 
 # Find closest pair
+def ClosestPair(bestCandidate, remainingPoints) {
+    if len(remainingPoints) != 0:
+        ClosestPair(bestCandidate, remainingPoints.)
+    else:
+}
 
 print(closestPair)

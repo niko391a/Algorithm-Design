ffad0a95c09cc91c1aa42f00084ea103eb933bb5
Nikolaj Alsøe <nals@itu.dk>
2026-09-19 13:19:18 +0200

    Update Closest-Pair.py

--- Week-04/Closest-Pair.py
+++ Week-04/Closest-Pair.py
@@ -11,9 +11,14 @@ points.sort()
 
 # Find closest pair
 def ClosestPair(bestCandidate, remainingPoints) {
-    if len(remainingPoints) != 0:
+    if len(remainingPoints) != 3:
+        # Split and recurse
         ClosestPair(bestCandidate, remainingPoints.)
+
+        # Combine
     else:
+        # Base case evaluate the 9 pairs no clean split for 3 points
+
 }
 
 print(closestPair)

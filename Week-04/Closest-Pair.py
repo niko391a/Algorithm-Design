3a109ba55d966d17c335ab9e4e5cce9266a9f003
Nikolaj Alsøe <nals@itu.dk>
2026-09-19 17:08:39 +0200

    Update Closest-Pair.py

--- Week-04/Closest-Pair.py
+++ Week-04/Closest-Pair.py
@@ -12,19 +12,38 @@ for _ in range(n):
 points.sort()
 
 # Find closest pair
-def ClosestPair(bestCandidate, remainingPoints) {
-    if len(remainingPoints) != 3:
+def ClosestPair(remainingPoints):
+    if len(remainingPoints) >= 4:
         # Split and recurse
-        ClosestPair(bestCandidate, remainingPoints.)
+        pointsSplit = len(remainingPoints)//2
+        # Left half
+        ClosestPair(remainingPoints[0:pointsSplit])
+        # Right half
+        ClosestPair(remainingPoints[pointsSplit:len(remainingPoints)])
 
         # Combine
     else:
         # Base case evaluate the 9 pairs no clean split for 3 points
-        branchCandidate = 0
+        branchLength = math.inf
+        branchCandidates = []
+        currentP1 = []
+        currentP2 = []
+        currentDistance = 0;
+
         for p1 in range(len(remainingPoints)):
+            currentP1 = remainingPoints[p1]
             for p2 in range(len(remainingPoints)):
-            euclideanDistane = math.sqrt(math.pow((p2[0]-p1[0]), 2) + math.pow((p2[1]-p1[1]), 2))    
+                if p1 != p2:
+                    currentP2 = remainingPoints[p2]
+                    currentDistance = EuclideanDistance(currentP1, currentP2)
+                    if currentDistance < branchLength: 
+                        branchCandidates = (currentP1, currentP2)
+                        branchLength = currentDistance
+        return branchCandidates
 
-}
+def EuclideanDistance(p1, p2):
+    return math.sqrt(math.pow((p2[0]-p1[0]), 2) + math.pow((p2[1]-p1[1]), 2))
 
-print(closestPair)
+
+
+print(ClosestPair([], points))
